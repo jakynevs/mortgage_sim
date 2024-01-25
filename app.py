dc5d7efc49b548c15c86c1e4d89e330d2468690b
jakynevs <johnnevilleis@hotmail.co.uk>
2024-01-25 17:32:36 +0100

    Created endpoint to add client data

--- app.py
+++ app.py
@@ -1,70 +1,49 @@
-import sqlite3
-from sqlite3 import Error
+from db_setup import create_connection
+from flask import Flask, jsonify, request
+# import sqlite3
 
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-    return conn
+app = Flask(__name__)
 
-# Remove this?
-# if __name__ == '__main__':
-#     create_connection('db/mortgage.db')
+@app.route('/client', methods=['POST'])
+def add_client():
+    # Info to add client
+    data = request.json
+    name = data.get('name')
+    dni = data.get('dni')
+    email = data.get('email')
+    requested_capital = data.get('requested_capital')
 
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
-        
-def main():
-    database = "db/mortgage.db"
-
-    # Check what can be null
-    # Check what are the best data types   
-    sql_create_clients_table = """ CREATE TABLE IF NOT EXISTS Client (
-                      client_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                      name TEXT NOT NULL,
-                      dni TEXT UNIQUE NOT NULL,
-                      email TEXT,
-                      requested_capital DECIMAL);
-                      """
-
-    sql_create_mortgage_sim_table = """ CREATE TABLE IF NOT EXISTS MortgageSimulation (
-                      mortgage_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                      client_id INTEGER NOT NULL,
-                      tae DECIMAL NOT NULL,
-                      repayment_term INTEGER NOT NULL,
-                      monthly_instalment DECIMAL NOT NULL,
-                      total DECIMAL NOT NULL,
-                      FOREIGN KEY (client_id) REFERENCES Client (id)
-                      );"""
+    # validate dni
+    # handle null values
     
-    # Create the db connection
-    conn = create_connection(database)
+    #Insert data to DB
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO Client (name, dni, email, requested_capital) VALUES (?, ?, ?, ?)',
+                   (name, dni, email, requested_capital))
+    conn.commit()
+    conn.close()
 
-    # Create the db tables
-    if conn is not None:
-        # Create client table
-        create_table(conn, sql_create_clients_table)
-        # Create mortgage table
-        create_table(conn, sql_create_mortgage_sim_table)
+    return jsonify({'message': 'Client added successfully'}), 201
     
-    else:
-        print("Error, cannot create db connection.")
+
+@app.route('/client/<client_id>', methods=['GET', 'PUT', 'DELETE'])
+def get_client():
+    # Info to retrieve client
+    
+    pass
+
+@app.route('/client/mortgage_sim/<client_id>', methods=['GET'])
+def get_mortgage_sim():
+    # Info to retrieve mortgage simulation of a given client
+    
+    pass
+
+# @app.route('/client/<client_id>', methods=['PUT'])
+# def get_client():
+#     # Info to retrieve client
+    
+#     pass
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)  # Start the Flask app
